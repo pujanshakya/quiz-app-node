69e925d79bcba53ddbd6844d540e1e3a8c576413
Pujan Shakya <pujanshakya18@gmail.com>
2025-08-05 10:06:55 +0545

    feat(user): addedd user type

---
+++ src/database/migrations/1754282895537-Migration.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1754282895537 implements MigrationInterface {
+    name = 'Migration1754282895537'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TYPE "public"."users_role_enum" AS ENUM('admin', 'user')
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "users"
+            ADD "role" "public"."users_role_enum" NOT NULL DEFAULT 'user'
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE "users" DROP COLUMN "role"
+        `);
+        await queryRunner.query(`
+            DROP TYPE "public"."users_role_enum"
+        `);
+    }
+
+}
